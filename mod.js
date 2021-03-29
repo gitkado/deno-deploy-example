a3255a386d5d8fb4b2795d4521daa1d9cb34d7e9
gitkado <gitkado@gmail.com>
2021-03-30 00:01:27 +0900

    feat: remove html, change mod.js

--- mod.js
+++ mod.js
@@ -1,45 +1,14 @@
-function handleRequest(request) {
-    const { pathname } = new URL(request.url);
-
-    if (pathname.startsWith("/html")) {
-        const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`;
-
-        return new Response(html, {
-            headers: {
-                // The interpretation of the body of the response by the client depends
-                // on the 'content-type' header.
-                // The "text/html" part implies to the client that the content is HTML
-                // and the "charset=UTF-8" part implies to the client that the content
-                // is encoded using UTF-8.
-                "content-type": "text/html; charset=UTF-8",
-            },
-        });
-    }
-
-    if (pathname.startsWith("/json")) {
-        // Use stringify function to convert javascript object to JSON string.
-        const json = JSON.stringify({
-            message: "Hello from Deno Deploy",
-        });
-
-        return new Response(json, {
-            headers: {
-                "content-type": "application/json; charset=UTF-8",
-            },
-        });
-    }
-
+function handleRequest() {
     return new Response(
-        `<body
+    `<body
       align="center"
       style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
     >
-      <p>
-        Visit <a href="/json">/json</a> for JSON response and
-        <a href="/html">/html</a> for HTML response
-      </p>
+        <h1>Hello world!</h1>
+        <div>
+            My name is gitkado. <a href="https://twitter.com/gitkado">@gitkado</a><br>
+            I really wanted to use JSX to write this page.
+        </div>
     </body>`,
         {
             headers: {
@@ -50,5 +19,5 @@ function handleRequest(request) {
 }
 
 addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event.request));
+    event.respondWith(handleRequest());
 });
